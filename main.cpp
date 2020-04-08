79bcbe5d5fb90f5aa9ffcdae7368d3eeccdd38a7
Zachary Michaels <mikezackles@gmail.com>
2020-04-07 17:49:17 -0700

    Add more minimal repro

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@
 int main(int, char **) {
   auto rope = boost::text::rope("Here's\nsome\ntext.");
   auto lines = boost::text::lines(rope);
+
+  // Uncomment the line below for a more minimal reproduction
+  //using foo = ranges::range_tag_of<decltype(lines)>;
+
+  // This was how I originally ran into this issue
   for (auto line : lines | ranges::views::drop(1)) std::cout << line;
   std::cout << std::endl;
   return EXIT_SUCCESS;
